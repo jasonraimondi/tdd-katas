4bd4dd3284d13d383de49dafb78a4e9407a08ee9
Jason Raimondi <jason@raimondi.us>
2018-08-11 15:08:54 -0700

    roman numeral solve

--- src/roman-numeral/roman-numeral.spec.ts
+++ src/roman-numeral/roman-numeral.spec.ts
@@ -17,15 +17,27 @@ describe('RomanNumerals', () => {
   describe('convert', () => {
     it('Should convert integer to Roman Numeral Output', () => {
       expect(romanNumerals.convert(1)).to.equal('I');
-      // expect(romanNumerals.convert(2)).to.equal('II');
+      expect(romanNumerals.convert(2)).to.equal('II');
+      expect(romanNumerals.convert(4)).to.equal('IV');
+      expect(romanNumerals.convert(5)).to.equal('V');
+      expect(romanNumerals.convert(6)).to.equal('VI');
+      expect(romanNumerals.convert(7)).to.equal('VII');
+      expect(romanNumerals.convert(8)).to.equal('VIII');
+      expect(romanNumerals.convert(9)).to.equal('IX');
+      expect(romanNumerals.convert(10)).to.equal('X');
+      expect(romanNumerals.convert(19)).to.equal('XIX');
+      expect(romanNumerals.convert(48)).to.equal('XLVIII');
+      expect(romanNumerals.convert(49)).to.equal('XLIX');
+      expect(romanNumerals.convert(90)).to.equal('XC');
+      expect(romanNumerals.convert(100)).to.equal('C');
     });
   });
 
-  // romanNumeralsData.forEach(({ input, output }) => {
-  //   describe('Convert All', () => {
-  //     it(`Should convert integer (${input}) to Roman Numeral Output`, () => {
-  //       expect(romanNumerals.convert(input)).to.equal(output);
-  //     });
-  //   });
-  // });
+  romanNumeralsData.forEach(({ input, output }) => {
+    describe('Convert All', () => {
+      it(`Should convert integer (${input}) to Roman Numeral Output`, () => {
+        expect(romanNumerals.convert(input)).to.equal(output);
+      });
+    });
+  });
 });
--- src/roman-numeral/roman-numeral.ts
+++ src/roman-numeral/roman-numeral.ts
@@ -1,9 +1,30 @@
 export class RomanNumerals {
   public convert(input: number): string {
-    if (input === 2) {
-      return 'II';
-    }
+    let output = '';
 
-    return 'I';
+    const map = [
+      {value: 1000, numeral: 'M'},
+      {value: 900, numeral: 'CM'},
+      {value: 500, numeral: 'D'},
+      {value: 400, numeral: 'CD'},
+      {value: 100, numeral: 'C'},
+      {value: 90, numeral: 'XC'},
+      {value: 50, numeral: 'L'},
+      {value: 40, numeral: 'XL'},
+      {value: 10, numeral: 'X'},
+      {value: 9, numeral: 'IX'},
+      {value: 5, numeral: 'V'},
+      {value: 4, numeral: 'IV'},
+      {value: 1, numeral: 'I'},
+    ];
+
+    map.forEach(({ value, numeral }) => {
+      while (input >= value) {
+        output += numeral;
+        input -= value;
+      }
+    });
+
+    return output;
   }
 }
